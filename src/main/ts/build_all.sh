ed7fa3d05ad88c82a67feadcdef4a3fcfc9d740d
lb584 <lb584@cam.ac.uk>
2021-03-27 15:56:46 +0000

    adding layout

--- src/main/ts/build_all.sh
+++ src/main/ts/build_all.sh
@@ -1,1 +1,1 @@
-./compile.sh;browserify src/App.js -o ../resources/static/bundle.js -s app;browserify src/AllPledgesPageCaller.js -o ../resources/static/pledges.js -s app
+./compile.sh;browserify org/network/App.js -o ../resources/static/bundle.js -s app;browserify org/network/AllPledgesPageCaller.js -o ../resources/static/pledges.js -s app
